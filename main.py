61fa0ea0abda6074f8857085c500c3312e5c75ed
Akash <akashdsimmons@gmail.com>
2025-06-19 11:19:39 -0400

    Fourth Commit - main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+from SavingsAccount import SavingsAccount
+from CheckingsAccount import CheckingAccount
+
+savings1 = SavingsAccount("Brendon Kaas", 2000, 50, "11111", "987654", 0.05)
+savings1.apply_interest()
+savings1.print_customer_information()
+
+print("\n")
+
+savings2 = SavingsAccount("Akash Simmons", 1500, 50, "22222", "987654", 0.04)
+savings2.apply_interest()
+savings2.print_customer_information()
+
+print("\n")
+
+checking1 = CheckingAccount("Professor Mohsen", 5000, 100, "33333", "987654", 1000)
+checking1.transfer(500, savings2)
+checking1.print_customer_information()
+print()
+savings2.print_customer_information()
+
+print("\n")
+
+checking2 = CheckingAccount("Brendon Kaas", 1000, 50, "44444", "987654", 300)
+checking2.transfer(500, savings1)
+checking2.print_customer_information()
